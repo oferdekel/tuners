da64bcdb768e8a4a1d624f9137ad7e44393a2fbd
Ofer Dekel <oferd@microsoft.com>
2019-05-27 22:26:39 -0700

    started impleneting greedy binary

---
+++ greedy_binary.py
@@ -0,0 +1,19 @@
+#   Project: Tuners
+#   Author: Ofer Dekel
+
+import numpy as np
+
+class GreedyBinarySearch():
+
+    def __init__(self, oracle):
+        self.oracle = oracle
+        self.current = np.zeros(oracle.dim)
+
+    def step():
+
+        
+        self.current = oracle.get_best()
+
+
+    def get_best:
+        return current
