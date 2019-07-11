f352ad4170ec9f3a2578a4284f5e4fc4e6812cc9
Ofer Dekel <oferd@microsoft.com>
2019-07-11 14:02:41 -0700

    added gaussian random

--- greedy_continuous.py
+++ greedy_continuous.py
@@ -15,12 +15,13 @@ class GreedyContinuousSearch():
         self.oracle.query([self.best_config])
         x, self.best_value = self.oracle.get_best(0).popitem()
 
-    def step(self, directions = np.empty(0)):
+    def step(self, directions, scale = 1, symmetrize = False):
         """ Takes a step in each of a given set of directions and commits to the step with the greatest improvement. """
 
-        if directions.size == 0:
-            I = np.eye(self.dimension)
-            directions = np.concatenate((I,-I))
+        if symmetrize:
+            directions = np.concatenate((directions,-directions))
+
+        directions = directions * scale
 
         previous_best_value = self.best_value
 
@@ -36,7 +37,29 @@ class GreedyContinuousSearch():
 
         return previous_best_value - self.best_value
 
+    def step_eye(self, scale = 1):
+        """ Takes a step in each of the cannonical directions and commits to the step with the greatest improvement. """
+
+        directions = np.eye(self.dimension)
+        return self.step(directions, scale, symmetrize = True)
+
+    def step_random_orthonormal(self, scale = 1):
+        """ Takes a step in each of an orthonormal set of directions and commits to the step with the greatest improvement. """
+
+        directions = randomBasis(self.dimension)
+        return self.step(directions, scale, symmetrize = True)
+
+    def step_random(self, scale = 1, num_directions = 0):
+        """ Takes a step in a given number of random directions and commits to the step with the greatest improvement. """
+
+        if num_directions == 0:
+            num_directions = self.dimension
+
+        directions = np.random.randn(num_directions, self.dimension)
+        return self.step(directions, scale, symmetrize = False)
+
     def get_best(self, tolerance=0):
+        """ Returns the best point observed so far. """
         return self.oracle.get_best(tolerance)
 
 
@@ -47,16 +70,14 @@ def main():
     POST_AMPLITUDE_SCALE = 0.45
     FREQUENCY_SCALE = 1
     STEPS = 10
+    STEP_SCALE = 0.2
 
     f = MultivariateSin(DIM, NUM_WAVES, PRE_AMPLITUDE_SCALE, POST_AMPLITUDE_SCALE, FREQUENCY_SCALE)
     o = CachingOracle(f)
     s = GreedyContinuousSearch(o)
 
-
     for i in range(STEPS):
-        b = randomBasis(DIM)
-        directions = np.concatenate((b,-b))
-        print(s.step(directions))
+        print(s.step_random(scale = STEP_SCALE))
 
 if __name__ == '__main__':
     main()
